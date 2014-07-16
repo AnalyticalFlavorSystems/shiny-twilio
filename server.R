0df6691ade455d5d3ffe959757c4941602c9282d
brewtech <evan@brewtech.us>
2014-07-16 09:28:29 -0400

    finished

--- server.R
+++ server.R
@@ -1,4 +1,46 @@
-shinyServer(function(input, output) {
-            
+require(XML)
+require(RCurl)
 
+shinyServer(function(input, output) {
+            ## Username get
+            username <- reactive({
+                ## In Shiny Server, you can use 'session$user' 
+                ## We just make it simple here for demo
+                "evandev"
+            })
+
+            observe({
+                if(!is.null(input$send_test_call)) {
+                    if(!is.null(isolate(input$phone_number)) &&
+                       isolate(input$phone_number) != "") {
+                        time <- now()
+
+                        ## We make the message
+                        message <- paste("Greetings, it is currently, ",time,".  This message can be made to say anything.")
+
+                        ## Now we build the XML file.
+                        xml_message <- newXMLNode("Response")
+                        newXMLNode("Say", message, attrs=c(voice='alice',
+                                                           language='en-gb', loop=3), parent=xml_message)
+
+                        ## Save the file temporarily
+                        ## We name the file based on the date and username
+                        filename <- paste0(Sys.Date(), "_", gsub(" ", "_", username(), ".xml"))
+                        f <- tempfile()
+
+                        saveXML(xml_message, file=f,
+                                prefix="<?xml version='1.0' encoding='UTF-8' ?>")
+                        
+                        ## Now we upload it to a server or other accessible area.
+                        ftpUpload(f, paste0("ftp://<your-ftp-server-name->",filename))
+
+                        ## Twilio RCurl Rest Call 
+                        POST('https://api.twilio.com/2010-04-01/Accounts/<Your-Twilio-account-id>/Calls.json',
+                             body = list(Url=paste0("https://<your-ftp-server-name>",filename),
+                                         From='+<Your-twilio-phone-number>',To=paste0("+1",isolate(input$phone_number))),
+                             config=authenticate("<Your-Twilio-account-id>",
+                                                 "<Your-Twilio-account-auth-token>",type="basic"))
+                    }
+                }
+            })
 })
--- ui.R
+++ ui.R
@@ -1,14 +1,19 @@
-shinyUI(bootstrapPage(
-    div(class="message_test",
-          div(class="center",
-                      textInput("phone_number", label="", value="")),
-          div(class="center",
-                      HTML("<div class='center'>
-                                       <button id='send_test_call' class='action-button btn btn-default '>
-                                                   Send test Call</button>
-                                                               </div>")
-           )
-      )
+shinyUI(fluidPage(
+          fluidRow(style="margin-top:80px;",
+                   column(4,offset=4,
+                div(class="message_test",
+                      div(style="text-align:center;",
+                          textInput("phone_number", label="Add Your Phone Number", value="")),
+                      div(class="center",
+                          HTML("<div style='text-align:center;'>
+                               <button id='send_test_call' class='action-button btn btn-primary '>
+                               Send test Call</button>
+                               </div>")
+                           )
+                      )
 
+                      )
+                )
 
-))
+        )
+)
